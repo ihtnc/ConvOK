5688fbd53f9578fca0207cf930a8d888b7717d06
ihtnc <ihopethisnamecounts@yahoo.com>
2013-06-18 23:45:51 -0700

    Tweaked the animations and added code comments

--- src/ConvOK_v2.h
+++ src/ConvOK_v2.h
@@ -1,11 +1,9 @@
 #include "pebble_os.h"
 
+//Uncomment this for debug mode
+//Debug mode treats the watch's minute value as the hour value and the watch's second value as the minute value.
+//This is to speed up the transition of the animations
 //#define DEBUG
-	
-// Slot on-screen layout:
-//     0
-//     1
-//     2
 
 #define SLOTS_COUNT 3
 #define SLOT_STATUS_EMPTY -1
@@ -18,22 +16,26 @@
 #define SLOT_TOP_YOFFSET 0
 #define SLOT_MID_YOFFSET 30
 #define SLOT_BOT_YOFFSET 104
-	
+
+//Duration and delay are in ms
+//Note: In debug mode, be aware that every 15 seconds, the top slot will change again after just 1000ms.
+//With this, ensure that the total animation duration of the top slot (out delay + out duration + in delay + in duration) does not reach 1000ms.
+//Otherwise the animation after every 15 seconds will get clunky.
 #define SLOT_TOP_OUT_DURATION 200
 #define SLOT_MID_OUT_DURATION 500
 #define SLOT_BOT_OUT_DURATION 400
 
 #define SLOT_TOP_OUT_DELAY 100
 #define SLOT_MID_OUT_DELAY 0
-#define SLOT_BOT_OUT_DELAY 50
+#define SLOT_BOT_OUT_DELAY 300
 	
 #define SLOT_TOP_IN_DURATION 300
 #define SLOT_MID_IN_DURATION 500
-#define SLOT_BOT_IN_DURATION 400
+#define SLOT_BOT_IN_DURATION 200
 
-#define SLOT_TOP_IN_DELAY SLOT_TOP_OUT_DELAY + SLOT_TOP_OUT_DURATION + 100
+#define SLOT_TOP_IN_DELAY SLOT_TOP_OUT_DELAY + SLOT_TOP_OUT_DURATION + 200
 #define SLOT_MID_IN_DELAY SLOT_MID_OUT_DELAY + SLOT_MID_OUT_DURATION + 0
-#define SLOT_BOT_IN_DELAY SLOT_BOT_OUT_DELAY + SLOT_BOT_OUT_DURATION + 50
+#define SLOT_BOT_IN_DELAY SLOT_BOT_OUT_DELAY + SLOT_BOT_OUT_DURATION + 100
 
 #define SCREEN_HEIGHT 168
 #define SCREEN_WIDTH 144
--- src/main.c
+++ src/main.c
@@ -12,31 +12,56 @@ PBL_APP_INFO(MY_UUID,
                "ConvOKv2-debug",
              #endif
              "ihopethisnamecounts",
-             1, 1, /* App version */
+             1, 2, /* App version */
              RESOURCE_ID_IMAGE_MENU_ICON,
              APP_INFO_WATCH_FACE);
 
+/*
+  This watchface has 3 slots: the fuzzy value, the hour value, and the quarter value.
+  
+  Slot on-screen layout:
+     0 - Fuzzy value   - "It is", "A little after", "A bit before", "Almost"
+     1 - Hour value    - Text value of the hour
+     2 - Quarter value - Text value of the quarter-hour
+  
+  Each slot animates when changed. 
+  The animation runs like this: the previous image in the slot will scroll out to the left of the screen and the current image in the slot will scroll in from the right of the screen to replace the previous image.
+  
+  Time formula:
+  X:00      = It is X o'Clock           X:30      = It is X thirty
+  X:01-X:06 = A little after X o'Clock  X:31-X:36 = A little after X thirty
+  X:07-X:11 = A bit before X fifteen    X:37-X:41 = A bit before X fortyfive
+  X:12-X:14 = Almost X fifteen          X:42-X:44 = Almost X fortyfive
+  X:15      = It is  X fifteen          X:45      = It is  X fortyfive
+  X:16-X:21 = A little after X fifteen  X:46-X:51 = A little after X fortyfive
+  X:22-X:26 = A bit before X thirty     X:52-X:56 = A bit before X+1 o'Clock
+  X:27-X:29 = Almost X thirty           X:57-X:59 = Almost X+1 o'Clock
+*/
+
 Window window;
 
+//These 2 image containers are needed for animation (1 for the current image and 1 for the previous image).
 BmpContainer image_containers[SLOTS_COUNT];
 BmpContainer previmage_containers[SLOTS_COUNT];
 
-GRect slot_rectangles[SLOTS_COUNT];
-GRect slot_out_rectangles[SLOTS_COUNT];
-GRect slot_in_rectangles[SLOTS_COUNT];
+GRect slot_rectangles[SLOTS_COUNT];     //Current frame
+GRect slot_out_rectangles[SLOTS_COUNT]; //Target frame of the scroll out animation
+GRect slot_in_rectangles[SLOTS_COUNT];  //Source frame of the scroll in animation
 
+//These 2 are used to animate the images (the current image is used on the in animation and the previous image is used in the out animation)
 PropertyAnimation slot_out_animations[SLOTS_COUNT];
 PropertyAnimation slot_in_animations[SLOTS_COUNT];
 
+//Animation timings
 const int SLOT_OUT_ANIMATION_DURATIONS[SLOTS_COUNT] = {SLOT_TOP_OUT_DURATION, SLOT_MID_OUT_DURATION, SLOT_BOT_OUT_DURATION};
 const int SLOT_OUT_ANIMATION_DELAYS[SLOTS_COUNT] = {SLOT_TOP_OUT_DELAY, SLOT_MID_OUT_DELAY, SLOT_BOT_OUT_DELAY};
-
 const int SLOT_IN_ANIMATION_DURATIONS[SLOTS_COUNT] = {SLOT_TOP_IN_DURATION, SLOT_MID_IN_DURATION, SLOT_BOT_IN_DURATION};
 const int SLOT_IN_ANIMATION_DELAYS[SLOTS_COUNT] = {SLOT_TOP_IN_DELAY, SLOT_MID_IN_DELAY, SLOT_BOT_IN_DELAY};
 
+//Position of the slots along the Y-axis
 const int SLOT_YOFFSETS[SLOTS_COUNT] = {SLOT_TOP_YOFFSET, SLOT_MID_YOFFSET, SLOT_BOT_YOFFSET};
 
-//The state is either "empty" or the image currently in the slot.
+//The state of the slot can either be "empty" or the image currently in the slot.
 //This is to prevent the slot from unnecessarily reloading the image.
 int image_slot_state[SLOTS_COUNT] = {SLOT_STATUS_EMPTY, SLOT_STATUS_EMPTY, SLOT_STATUS_EMPTY};
 int previmage_slot_state[SLOTS_COUNT] = {SLOT_STATUS_EMPTY, SLOT_STATUS_EMPTY, SLOT_STATUS_EMPTY};
@@ -69,17 +94,7 @@ void load_image_to_slot(int slot_number, int hour_value, int minute_value)
 {
   //Loads the digit image from the application's resources and displays it on-screen in the correct location.
   
-  //Time formula:
-  //X:00      = It is X o'Clock           X:30      = It is X thirty
-  //X:01-X:06 = A little after X o'Clock  X:31-X:36 = A little after X thirty
-  //X:07-X:11 = A bit before X fifteen    X:37-X:41 = A bit before X fortyfive
-  //X:12-X:14 = Almost X fifteen          X:42-X:44 = Almost X fortyfive
-  //X:15      = It is  X fifteen          X:45      = It is  X fortyfive
-  //X:16-X:21 = A little after X fifteen  X:46-X:51 = A little after X fortyfive
-  //X:22-X:26 = A bit before X thirty     X:52-X:56 = A bit before X+1 o'Clock
-  //X:27-X:29 = Almost X thirty           X:57-X:59 = Almost X+1 o'Clock
-  
-  //TODO: Signal these error(s)?
+  //Validations
   if (slot_number < 0 || slot_number > SLOTS_COUNT) { return; }
   if (hour_value < 0 || hour_value > 11) { return; }
   if (minute_value < 0 || minute_value > 59) { return; }
@@ -88,48 +103,55 @@ void load_image_to_slot(int slot_number, int hour_value, int minute_value)
   int quarter_value = minute_value / 15;
   int quarter_remainder = minute_value % 15;
 
+  //Determine the slot value (the index of the correct image resource)
   if (slot_number == SLOT_TOP)
   {
     if (quarter_remainder == 0)
 	{
-      slot_value = 0;
+      slot_value = 0; //Index of "It is"
     }
     else if(quarter_remainder >= 1 && quarter_remainder <= 6)
 	{
-      slot_value = 1;
+      slot_value = 1; //Index of "A little after"
     }
     else if(quarter_remainder >= 7 && quarter_remainder <= 11)
 	{
-      slot_value = 2;
+      slot_value = 2; //Index of "A bit before"
     }
     else if(quarter_remainder >= 12 && quarter_remainder <= 14)
 	{
-      slot_value = 3;
+      slot_value = 3; //Index of "Almost"
     }
   }
   else if (slot_number == SLOT_MID)
   {
+    //After 6 seconds, the fuzzy value will now pertain to the next quarter value.
+    //So, if it's currently the third quarter, the hour should also move forward.
 	if (quarter_remainder >= 7 && quarter_value == 3) { slot_value = hour_value + 1; }
     else { slot_value = hour_value; }
 	  
-	//Normalize value (should only be 0-11)
+	//Normalize the value (should only be 0-11)
 	slot_value = slot_value % 12;
   }
   else if (slot_number == SLOT_BOT) 
   {
+	//After 6 seconds, the fuzzy value will now pertain to the next quarter value.
 	if (quarter_remainder >= 7) { slot_value = quarter_value + 1; }
     else { slot_value = quarter_value; }
     
-    //Normalize value (should only be 0-3)
+    //Normalize the value (should only be 0-3)
 	slot_value = slot_value % 4; 
   }
   
   //Do not reload the image if slot_state value did not change
   if (image_slot_state[slot_number] == slot_value) { return; }
   
+  //The state is about to change so the current state will become the previous state
   int prevslot_state = image_slot_state[slot_number];
+  
   unload_image_from_slot(slot_number);
   
+  //Load the image based on the current state
   int resourceid = determine_image_from_value(slot_number, slot_value);
   bmp_init_container(resourceid, &image_containers[slot_number]);
   image_containers[slot_number].layer.layer.frame.origin.x = SLOT_XOFFSET + SCREEN_WIDTH;
@@ -137,12 +159,14 @@ void load_image_to_slot(int slot_number, int hour_value, int minute_value)
   layer_add_child(&window.layer, &image_containers[slot_number].layer.layer);
   
   image_slot_state[slot_number] = slot_value;  
-	
+  
+  //Run the out animation only if there is a previous image.
+  //This is applicable when the watchface runs the first time.
   bool has_out_animation = false;
   if(prevslot_state != SLOT_STATUS_EMPTY)
   {
+    //Load the image based on the previous state
     int prevresourceid = determine_image_from_value(slot_number, prevslot_state);
-      
     bmp_init_container(prevresourceid, &previmage_containers[slot_number]);
     previmage_containers[slot_number].layer.layer.frame.origin.x = SLOT_XOFFSET;
     previmage_containers[slot_number].layer.layer.frame.origin.y =  SLOT_YOFFSETS[slot_number];
@@ -176,7 +200,7 @@ int determine_image_from_value(int slot_number, int slot_value)
 									 
 void unload_image_from_slot(int slot_number) 
 {
-  //Removes the digit from the display and unloads the image resource to free up RAM.
+  //Removes the images from the display and unloads the resource to free up RAM.
   //Can handle being called on an already empty slot.
 
   if (image_slot_state[slot_number] != SLOT_STATUS_EMPTY) 
@@ -215,13 +239,12 @@ void slot_out_animation_stopped(Animation *animation, void *data)
 void slot_in_animation_stopped(Animation *animation, void *data)
 {
   (void)animation;
-  Layer *current = (Layer *)data;
-	
-  current->frame.origin.x = SLOT_XOFFSET;
+  (void)data;
 }
 
 void animate_slot(int slot_number, bool out_animation)
 {
+  //Do not run the animation if there are no images.
   if(image_slot_state[slot_number] == SLOT_STATUS_EMPTY) { return; }
 
   property_animation_init_layer_frame(&slot_out_animations[slot_number], 
@@ -235,7 +258,7 @@ void animate_slot(int slot_number, bool out_animation)
                          {
                            .stopped = (AnimationStoppedHandler)slot_out_animation_stopped
                          }, 
-                         (void *)(&previmage_containers[slot_number].layer.layer));
+                         NULL);
   animation_set_delay(&slot_out_animations[slot_number].animation, SLOT_OUT_ANIMATION_DELAYS[slot_number]);
   animation_schedule(&slot_out_animations[slot_number].animation);
   
@@ -250,7 +273,7 @@ void animate_slot(int slot_number, bool out_animation)
                          {
                            .stopped = (AnimationStoppedHandler)slot_in_animation_stopped
                          }, 
-                         (void *)(&image_containers[slot_number].layer.layer));
+                         NULL);
   animation_set_delay(&slot_in_animations[slot_number].animation, SLOT_IN_ANIMATION_DELAYS[slot_number]);
   animation_schedule(&slot_in_animations[slot_number].animation);
 }
@@ -297,6 +320,7 @@ void handle_minute_tick(AppContextRef ctx, PebbleTickEvent *t)
 {
   (void)t;
   (void)ctx;
+
   display_time(t->tick_time);
 }
 
